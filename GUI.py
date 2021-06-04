4cd273f479edda280cbf68d05156bf36f4b35cac
Abhishek Suman <iabhishek.suman1993@gmail.com>
2021-06-04 17:13:53 -0500

    cli v1

--- GUI.py
+++ GUI.py
@@ -1,3 +1,11 @@
-import pygame
-import time
-pygame.font.init()
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+display = pygame.display.set_mode([400, 400])
+pygame.display.set_caption('trial one')
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
--- generator.py
+++ generator.py
@@ -7,7 +7,12 @@ class SudokuGenerator(SudokuSolver):
         super().__init__(seed)
         self.difficulty = difficulty
         self.unsolved_board, self.solved_board = self.generator(self.difficulty)
-        self.unsolved_board_copy = self.unsolved_board.copy()
+        self.unsolved_board_copy = [self.unsolved_board.copy(), self.unsolved_board.copy()]
+
+    def copy(self):
+        self.unsolved_board_copy.append(self.unsolved_board_copy[-1].copy())
+        if len(self.unsolved_board_copy) > 2:
+            self.unsolved_board_copy.pop(0)
 
     def generator(self, difficulty):
         np.random.seed = self.seed
@@ -26,3 +31,20 @@ class SudokuGenerator(SudokuSolver):
         np.put(sudoku_unsolved_grid, np.random.choice(idx, size=N, replace=False), 0)
 
         return sudoku_unsolved_grid, sudoku_solved_grid
+
+    def print_grid(self):
+        print('\n'+ '~'*11, 'Lets play Sudoku!', '~'*11, '\n')
+        print('*' + '-' * 39 + '*')
+        for row in range(9):
+            print('||',
+                  ' | '.join(str(i).replace('0',' ') for i in self.unsolved_board_copy[-1][row][:3]), "||",
+                  ' | '.join(str(i).replace('0', ' ') for i in self.unsolved_board_copy[-1][row][3:6]),"||",
+                  ' | '.join(str(i).replace('0', ' ') for i in self.unsolved_board_copy[-1][row][6:]),
+                  '||')
+            if row in [2, 5]:
+                print('*' + '-' * 12 + '||'+ '-' * 11 + '||'+ '-' * 12 + '*')
+        print('*' + '-' * 39 + '*')
+
+
+sudoku = SudokuGenerator()
+sudoku.print_grid()
